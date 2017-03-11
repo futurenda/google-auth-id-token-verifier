4f4d92439e9623b68325f1e4c44eb193d481af2a
Zeno Zeng <zenoofzeng@gmail.com>
2017-03-11 14:45:23 +0800

    cache control for getFederatedSignonCerts

--- certs.go
+++ certs.go
@@ -3,6 +3,8 @@ package googleAuth
 import (
 	"crypto/rsa"
 	"net/http"
+	"regexp"
+	"strconv"
 	"time"
 )
 
@@ -11,8 +13,39 @@ type Certs struct {
 	Expiry time.Time
 }
 
-func getFederatedSignonCerts() error {
+var (
+	certs *Certs
+)
+
+func getFederatedSignonCerts() (*Certs, error) {
+	if certs != nil {
+		if time.Now().Before(certs.Expiry) {
+			return certs, nil
+		}
+	}
 	resp, err := http.Get(googleOAuth2FederatedSignonCertsURL)
-	resp.Header.Get("")
-	return nil
+	if err != nil {
+		return nil, err
+	}
+	cacheControl := resp.Header.Get("cache-control")
+	cacheAge := int64(3600) // Set default cacheAge to 1 hour
+	if len(cacheControl) > 0 {
+		re := regexp.MustCompile("max-age=([0-9]*)")
+		match := re.FindAllStringSubmatch(cacheControl, -1)
+		if len(match) > 0 {
+			if len(match[0]) == 2 {
+				maxAge := match[0][1]
+				maxAgeInt, err := strconv.ParseInt(maxAge, 10, 64)
+				if err != nil {
+					return nil, err
+				}
+				cacheAge = maxAgeInt
+			}
+		}
+	}
+	certs = &Certs{
+		Expiry: time.Now().Add(time.Second * time.Duration(cacheAge)),
+	}
+
+	return nil, nil
 }
--- validator.go
+++ validator.go
@@ -18,3 +18,5 @@ var (
 		"https://accounts.google.com",
 	}
 )
+
+type Validator struct{}
