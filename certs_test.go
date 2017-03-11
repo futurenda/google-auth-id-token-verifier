19089f152e4d5174aae612d0e7f8d90e08130c56
Zeno Zeng <zenoofzeng@gmail.com>
2017-03-11 15:15:25 +0800

    test for 9e84f33bf244380eb676d35db27a49f86d7b2235

--- certs_test.go
+++ certs_test.go
@@ -16,4 +16,9 @@ func TestGetFederatedSignonCerts(t *testing.T) {
 	if cacheAge <= 7200 {
 		t.Error("max-age not found")
 	}
+
+	key := certs.Keys["9e84f33bf244380eb676d35db27a49f86d7b2235"]
+	if key == nil {
+		t.Error("9e84f33bf244380eb676d35db27a49f86d7b2235 should exists")
+	}
 }
